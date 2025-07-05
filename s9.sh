73259c368c1e78c67631f1fd24fc526372f15980
WonHyung Shin <wonhyung64@icloud.com>
2025-07-05 17:41:46 +0900

    feat - recon

--- s9.sh
+++ s9.sh
@@ -186,13 +186,13 @@ experiments=(
 # "--embedding-k=64--depth=2 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192"
 
 # "--embedding-k=8--depth=0 --base-model=ncf --dataset-name=coat --batch-size=4096"
-"--embedding-k=8--depth=0 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192"
+"--embedding-k=8-- depth=0 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192"
    
 # "--embedding-k=8--base-model=mf --dataset-name=coat --batch-size=4096"
-"--embedding-k=8--base-model=mf --dataset-name=yahoo_r3 --batch-size=8192"
+"--embedding-k=8-- base-model=mf --dataset-name=yahoo_r3 --batch-size=8192"
    
 # "--embedding-k=8--depth=2 --base-model=ncf --dataset-name=coat --batch-size=4096"
-"--embedding-k=8--depth=2 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192"
+"--embedding-k=8-- depth=2 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192"
 
 )
 
