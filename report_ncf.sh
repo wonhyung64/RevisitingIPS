7475f1598cefdbf957794065705026351e3c8fb0
WonHyung Shin <wonhyung64@icloud.com>
2025-08-14 19:31:00 +0900

    feat - reconstruction

---
+++ report_ncf.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+ENV=python
+RANDOM_SEED=0
+
+TASK=interaction
+
+experiments=(
+
+#coat
+# "$TASK/ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-4 --weight-decay=1e-4" 
+# "$TASK/akb_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-3 --weight-decay=1e-5 --G=1 --lr2=0.05 --lr3=0.05 --J=3 --gamma=1. --C=1e-5 --lamb2=0. --lamb3=0." 
+# "$TASK/naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-4 --weight-decay=1e-4" 
+# "$TASK/esmm.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-4 --weight-decay=1e-4 --lambda1=0.1" 
+# "$TASK/multi_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-3 --weight-decay=1e-5 --lambda1=2." 
+# "$TASK/multi_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-3 --weight-decay=1e-4 --lambda1=2." 
+# "$TASK/escm2_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-3 --weight-decay=1e-4 --lambda1=2. --lambda2=1." 
+# "$TASK/escm2_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-3 --weight-decay=1e-4 --lambda1=2. --lambda2=1." 
+# "$TASK/ips_v2.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-4 --weight-decay=1e-4 --lambda1=1. --lambda2=1. --lambda3=1." 
+# "$TASK/naive_v2.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=coat --batch-size=4096 --lr=1e-4 --weight-decay=1e-4 --lambda1=1. --lambda2=1. --lambda3=1." 
+
+#yahoo
+# "$TASK/ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-2 --weight-decay=1e-4"
+# "$TASK/akb_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-4 --weight-decay=1e-4 --G=1 --lr2=0.05 --lr3=0.05 --J=3 --gamma=1. --C=1e-5 --lamb2=0. --lamb3=0."
+# "$TASK/naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-4 --weight-decay=1e-5"
+# "$TASK/esmm.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-2 --weight-decay=1e-6 --lambda1=2."
+# "$TASK/multi_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-4 --weight-decay=1e-6 --lambda1=1."
+# "$TASK/multi_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-4 --weight-decay=1e-5 --lambda1=2."
+# "$TASK/escm2_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-4 --weight-decay=1e-4 --lambda1=2. --lambda2=1."
+# "$TASK/escm2_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-4 --weight-decay=1e-4 --lambda1=2. --lambda2=1."
+# "$TASK/ips_v2.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-4 --weight-decay=1e-4"
+# "$TASK/naive_v2.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e-4 --weight-decay=1e-6"
+
+#KuaiRec
+# "$TASK/ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=KuaiRec --batch-size=8192 --lr=1e-4 --weight-decay=1e-4" 
+# "$TASK/naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=KuaiRec --batch-size=8192 --lr=1e-3 --weight-decay=1e-6" 
+# "$TASK/multi_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=KuaiRec --batch-size=8192 --lr=1e-3 --weight-decay=1e-6 --lambda1=1." 
+# "$TASK/esmm.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=KuaiRec --batch-size=8192 --lr=1e-3 --weight-decay=1e-6 --lambda1=1."  
+# "$TASK/akb_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e- --weight-decay=1e- --G=1 --lr2=0.05 --lr3=0.05 --J=3 --gamma=1. --C=1e-5 --lamb2=0. --lamb3=0." 
+# "$TASK/multi_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e- --weight-decay=1e- --lambda1=" 
+# "$TASK/escm2_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e- --weight-decay=1e- --lambda1= --lambda2=" 
+# "$TASK/escm2_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e- --weight-decay=1e- --lambda1= --lambda2=" 
+# "$TASK/ips_v2.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e- --weight-decay=1e- --lambda1= --lambda2= --lambda3=" 
+# "$TASK/naive_v2.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=yahoo_r3 --batch-size=8192 --lr=1e- --weight-decay=1e- --lambda1= --lambda2= --lambda3=" 
+
+
+)
+
+for index in ${!experiments[*]}; do
+    echo ${experiments[$index]} --data-dir=$TASK/data --random-seed=$RANDOM_SEED 
+    $ENV ${experiments[$index]} --data-dir=$TASK/data --random-seed=$RANDOM_SEED &
+done
+
+
+TASK=causality
+
+
+experiments=(
+
+#original
+#emb64
+# "$TASK/ips_plus.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-2 --weight-decay=1e-4 --propensity=pred" 
+# "$TASK/akb_ips_plus.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-4 --weight-decay=1e-4 --G=1 --lr2=0.05 --lr3=0.05 --lamb2=0. --lamb3=0. --gamma=1. --J=3 --C=1e-5" 
+# "$TASK/naive_plus.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-2 --weight-decay=1e-5" 
+# "$TASK/esmm_plus.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-4 --weight-decay=1e-4 --lambda1=1." 
+# "$TASK/multi_plus_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-3 --weight-decay=1e-4 --lambda1=0.001 --propensity=pred" 
+# "$TASK/multi_plus_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-3 --weight-decay=1e-4 --lambda1=1." 
+# "$TASK/escm2_plus_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-2 --weight-decay=1e-4 --lambda1=0.01 --lambda2=2. --propensity=pred" 
+# "$TASK/escm2_plus_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-4 --weight-decay=1e-4 --lambda1=2. --lambda2=2." 
+# "$TASK/v2_plus_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-2 --weight-decay=1e-4 --lambda1=0.001 --lambda2=2. --lambda3=0.01 --propensity=pred" 
+# "$TASK/v2_plus_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=original --lr=1e-4 --weight-decay=1e-4 --lambda1=2. --lambda2=2. --lambda3=1." 
+
+#personalized
+# "$TASK/ips_plus.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-2 --weight-decay=1e-4 --propensity=pred" 
+# "$TASK/akb_ips_plus.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-2 --weight-decay=1e-4 --G=1 --lr2=0.05 --lr3=0.05 --lamb2=0. --lamb3=0. --gamma=1. --J=3 --C=1e-5" 
+# "$TASK/naive_plus.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-2 --weight-decay=1e-5" 
+# "$TASK/esmm_plus.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-2 --weight-decay=1e-5 --lambda1=2." 
+# "$TASK/multi_plus_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-2 --weight-decay=1e-4 --lambda1=0.01 --propensity=pred" 
+# "$TASK/multi_plus_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-4 --weight-decay=1e-4 --lambda1=1." 
+# "$TASK/escm2_plus_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-2 --weight-decay=1e-4 --lambda1=1. --lambda2=2. --propensity=pred" 
+# "$TASK/escm2_plus_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-3 --weight-decay=1e-4 --lambda1=1. --lambda2=2." 
+# "$TASK/v2_plus_ips.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-2 --weight-decay=1e-4 --lambda1=1. --lambda2=2. --lambda3=1. --propensity=pred" 
+# "$TASK/v2_plus_naive.py --depth=0 --embedding-k=64 --base-model=ncf --dataset-name=personalized --lr=1e-2 --weight-decay=1e-5 --lambda1=1. --lambda2=2. --lambda3=1." 
+
+)
+
+for index in ${!experiments[*]}; do
+    echo ${experiments[$index]} --data-dir=$TASK/data --random-seed=$RANDOM_SEED 
+    $ENV ${experiments[$index]} --data-dir=$TASK/data --random-seed=$RANDOM_SEED &
+done
--- report_true_ps.sh
+++ report_true_ps.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-# Function to check the number of running processes
-check_jobs() {
-    jobs -r | wc -l
-}
-
-MAX_JOBS=4 # Maximum number of parallel jobs
-COUNTER=0 # 전체 제출 카운터
-
-# ENV=python
-ENV=/home1/wonhyung64/anaconda3/envs/openmmlab/bin/python3
-
+ENV=python
 RANDOM_SEED=0
 
 TASK=causality
-
 
 experiments=(
 
@@ -85,20 +74,6 @@ experiments=(
 )
 
 for index in ${!experiments[*]}; do
-    # 최대 병렬 프로세스가 다 돌고 있으면 대기
-    while [ "$(check_jobs)" -ge "$MAX_JOBS" ]; do
-        echo "Max jobs ($MAX_JOBS) running. Waiting..."
-        sleep 1m
-    done
-
-    # GPU 할당 (round-robin)
-    GPU_ID=$(( COUNTER % 4 ))   # 0,1,2,3 반복
-    export CUDA_VISIBLE_DEVICES=$GPU_ID
-
-    echo "Launching on GPU $GPU_ID: "
     echo ${experiments[$index]} --data-dir=$TASK/data 
     $ENV ${experiments[$index]} --data-dir=$TASK/data &
-
-    (( COUNTER++ ))
-    sleep 5
 done
